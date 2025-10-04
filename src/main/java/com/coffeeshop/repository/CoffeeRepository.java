5f05080735f67b1552d818c9bac2eb8e5b6f4a7e
anhthi <at06012005@gmail.com>
2025-10-04 17:13:19 +0700

    Xóa toàn bộ code trên nhánh main

--- src/main/java/com/coffeeshop/repository/CoffeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.coffeeshop.repository;
-
-import com.coffeeshop.model.Coffee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CoffeeRepository extends JpaRepository<Coffee, Long> {
-}
